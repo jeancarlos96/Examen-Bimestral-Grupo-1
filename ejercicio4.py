698c25edc9f1777b340de98938ff109a35fbb6b1
Javier <javich_dj@hotmail.com>
2016-06-09 08:00:05 -0500

    implementacion del txt
    se aumenta las funciones del texto

--- ejercicio4.py
+++ ejercicio4.py
@@ -2,6 +2,15 @@ from textblob import TextBlob
 
 frase = input("INGRESE SU FRASE: ")
 
+def creartxt():
+	texto_trad = open ('datos.txt','a')
+	texto_trad.close()
+
+def grabartxt():
+	texto_trad = open('datos.txt','w')
+	txt = texto_trad.read()
+	texto_trad.write(txt, 'w')	
+	archi.close()
 
 blob1 = TextBlob(frase)
 blob1.tags
@@ -11,4 +20,7 @@ print ("#############-------- TRADUCCION ----##########################")
 print (blob1.translate (to = "en")) ##"en" en esta parte se pone el idionma al que se quiere traducir
 #for sentence in blob1.sentences:
 #	print(sentence.sentiment.polarity)
- 	
+
+
+creartxt()
+grabartxt()
