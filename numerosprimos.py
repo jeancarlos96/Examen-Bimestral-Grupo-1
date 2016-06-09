669d6bd17c45f21f69ed782b4f08cb0c7be6f44f
Karla Arias <karliz_steffania@hotmail.com>
2016-06-09 07:21:46 -0500

    Comparación
    Comparación

--- numerosprimos.py
+++ numerosprimos.py
@@ -1,3 +1,13 @@
 def nprimos():
 	num=int(input("Ingresa un entero, por favor: "))
-	
+	if num<2:
+		return False
+	for x in range(2,num):
+		if num %i ==0:
+			return False
+	return True
+
+
+print es_primo(3)    #para probarlo llamamos a la función
+
+nprimos():
