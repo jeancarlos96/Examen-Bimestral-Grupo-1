6f60e966a30b5f3b3da33fa8455a3c165dcc0188
Karla Arias <karliz_steffania@hotmail.com>
2016-06-09 08:49:28 -0500

    Ejercicio

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,19 +1,30 @@
-num= input("inserta un numero: ")
+num= input("Ingrese un número: ")
 numero = int(num)
-c=2
 contador = 0
 verificar= False
 for i in range(1,numero+1):
-	if (numero% i)==0:
+
+	while (numero% i)==0:
 		contador = contador + 1
+		print (contador)
 	if contador >= 3:
 		verificar=True
 		break
 
-
 if contador==2 or verificar==False:
 	print ("El número es primo")
-	print ("La secuencia es:" )
-
+		print ()
 else: 
 	print ("El numero no es primo")
+
+def creartxt():
+	archi=open('primos.txt', 'w')
+	archi.close()
+
+def grabartxt():
+	archi=open('primos.txt','a')
+	archi.write(numero)
+	archi.close()
+
+creartxt()
+grabartxt()
