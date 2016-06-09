d57d580de74c41d0a2614a7e3616d3aebc97861e
jeancarlos96 <jeancarloschamba@hotmail.com>
2016-06-09 07:33:23 -0500

    avance-ejercicio4
    traduccion,  falta pedir la palabra al usuario

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,1 +1,13 @@
-dghszfd
+from textblob import TextBlob
+
+text = 'Hola'
+
+blob1 = TextBlob(text)
+blob1.tags
+blob1.noun_phrases
+
+print ("#############-------- TRADUCCION ----##########################")
+print (blob1.translate (to = "en")) ##"en" en esta parte se pone el idionma al que se quiere traducir
+#for sentence in blob1.sentences:
+#	print(sentence.sentiment.polarity)
+ 	
